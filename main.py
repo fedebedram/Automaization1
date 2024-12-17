840ce42c0bf9786595052d70ddbdb6516b5707a2
Federico Bedolla Ramos <fede.bedram@gmail.com>
2024-12-16 21:40:00 -0600

    Quick check, updating VSC

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import pandas as pd
-from sqlalchemy import create_engine
 import datetime
+import pandas as pd
+
+raw_data = pd.read_csv("../Raw_Data/EGRE_DATOS_ABIERTOS_2020.csv")
+print(raw_data)
